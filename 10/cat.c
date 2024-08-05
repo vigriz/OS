07d5f1789d7443909779e8acddfaee6b7d5d5f31
Vigriz <142087645+vigriz@users.noreply.github.com>
2024-08-05 19:55:56 +0530

    Update cat.c

--- 10/cat.c
+++ 10/cat.c
@@ -1,1 +1,49 @@
+//compile with gcc -o my_cat my_cat.c
+//Run the compiled program with a filename as an argument: ./my_cat somefile.txt
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    int fd;
+    char buffer[BUFFER_SIZE];
+    ssize_t bytesRead;
+
+    // Open the file
+    fd = open(argv[1], O_RDONLY);
+    if (fd < 0) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    // Read and write the file contents
+    while ((bytesRead = read(fd, buffer, BUFFER_SIZE)) > 0) {
+        if (write(STDOUT_FILENO, buffer, bytesRead) < 0) {
+            perror("Error writing to stdout");
+            close(fd);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Check for read error
+    if (bytesRead < 0) {
+        perror("Error reading file");
+    }
+
+    // Close the file
+    if (close(fd) < 0) {
+        perror("Error closing file");
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
