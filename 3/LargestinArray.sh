7b8f40717452cb58b9c9a86ead23556c27724354
Vigriz <142087645+vigriz@users.noreply.github.com>
2024-08-28 19:00:56 +0530

    Update LargestinArray.sh

--- 3/LargestinArray.sh
+++ 3/LargestinArray.sh
@@ -17,3 +17,13 @@ for ((i=1; i<size; i++)); do
 done
 
 echo "Largest Number in array: $big"
+
+Output:
+Array Size>> 5
+arr[0]: 12
+arr[1]: 7
+arr[2]: 22
+arr[3]: 5
+arr[4]: 18
+
+Largest Number in array: 22
