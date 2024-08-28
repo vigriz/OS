6e079d30b6f34d6d1c0bea9315b033a01b3a55cf
Vigriz <142087645+vigriz@users.noreply.github.com>
2024-08-28 19:32:14 +0530

    Update numPalindrome.sh

--- 9/numPalindrome.sh
+++ 9/numPalindrome.sh
@@ -10,3 +10,16 @@ then
 else
 	echo "Not a palindrome"
 fi 
+
+
+Output:
+chmod +x check_palindrome.sh
+./check_palindrome.sh
+Enter number>> 12321
+Palindrome
+Enter number>> 12345
+Not a palindrome
+Enter number>> 7
+Palindrome
+Enter number>> 01210
+Palindrome
